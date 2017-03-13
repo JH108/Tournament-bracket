de889ec0b347c129f2b0787e1691e5579ddfe25a
Jesse Hill <jsnahill@gmail.com>
2017-03-13 16:14:35 -0700

    add factory. Add two factory methods to get teams and add a team. Integrate factory functions into controller

--- client/app.js
+++ client/app.js
@@ -1,8 +1,40 @@
 angular.module('tournament', [])
-  .controller('enterTeams', function($scope) {
-    $scope.teams = [];
+  .controller('enterTeams', function($scope, Teams) {
+
+    Teams.getAllTeams().then(function(teams) {
+      // console.log(teams)
+      $scope.teams = teams;
+    });
 
     $scope.addTeam = function(team) {
-      $scope.teams.push({name: team});
+      Teams.addNewTeam({name: team});
+      //$scope.teams.push({name: team});
     }
+
+  })
+  .factory('Teams', function($http) {
+
+    var getAllTeams = function() {
+      return $http({
+        method: 'GET',
+        url: '/'
+      }).then(function(res) {
+        console.log('console log is here', res);
+        return res.data;
+      });
+    };
+
+    var addNewTeam = function(team) {
+      return $http({
+        method: 'POST',
+        url: '/',
+        data: team
+      });
+    }
+
+    return {
+      getAllTeams: getAllTeams,
+      addNewTeam: addNewTeam
+    };
+
   })

92f093176bfde2c0952c774cc8cba3773b03ee0d
Jesse Hill <jsnahill@gmail.com>
2017-03-13 15:22:03 -0700

    refactor server to use process.env.ip for when app is deployed

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 3000;
+var ip = process.env.IP || '127.0.0.1';
 
 app.use(bodyParser.json());
-app.use(express.static('client/index.html'));
+app.use(express.static('client'));
 
-app.listen(port);
+app.listen(port, ip);
 console.log('Server is listening at port: ' + port);

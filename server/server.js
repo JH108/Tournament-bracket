619eae31fec09c392f0e0ffbdde76fc4a9c015b5
Jesse Hill <jsnahill@gmail.com>
2017-03-13 16:12:56 -0700

    add get, post routes. Add temporary teams data to check if the routes are working correctly

--- server/server.js
+++ server/server.js
@@ -6,8 +6,34 @@ var app = express();
 var port = process.env.PORT || 3000;
 var ip = process.env.IP || '127.0.0.1';
 
+var teams = [
+  {
+    name: 'CHSM'
+  },
+  {
+    name: 'HCYA'
+  },
+  {
+    name: 'Golden State'
+  }
+];
+
 app.use(bodyParser.json());
 app.use(express.static('client'));
+
+app.get('/', function(req, res) {
+  console.log(teams)
+  res.send(200, teams);
+  res.end(teams);
+});
+
+app.post('/', function(req, res) {
+  var team = req.body.data;
+  teams.push(team);
+  console.log(req.body.data);
+  res.send(201);
+  res.end();
+})
 
 app.listen(port, ip);
 console.log('Server is listening at port: ' + port);

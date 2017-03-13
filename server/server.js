85c96e6aebfc7bd59268fb20ff57b216a57e123c
Jesse Hill <jsnahill@gmail.com>
2017-03-13 15:14:46 -0700

    finish hooking up express server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ var bodyParser = require('body-parser');
 
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(express.static('client/index.html'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(port);
+console.log('Server is listening at port: ' + port);
